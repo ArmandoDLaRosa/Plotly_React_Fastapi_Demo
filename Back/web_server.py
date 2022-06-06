6a049977d16ef3d9a584516034e9c55aa86b10ae
armando <a01570376@itesm.mx>
2022-06-06 13:34:29 -0500

    🔧  gitignore for back

--- Back/web_server.py
+++ Back/web_server.py
@@ -3,6 +3,7 @@ import urllib
 
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.wsgi import WSGIMiddleware
@@ -36,6 +37,26 @@ def read_main():
 @app.get("/status")
 def get_status():
     return {"status": "ok"}
+
+@app.get("/graph-demo")
+def get_graph():
+    details = {
+        'MeterID': [ "METER_01EP7CKAY1EDQJ3VIR8TQ3DT2 ",
+                    "METER_01EP7JPM10WSLN6GO4RSYHVL8  ",
+                    "METER_01EP7KFQR1E1YGEU94H2WX81L  ",
+                    "METER_01EP7WWUQHL6UZH14KDC612WF  ",
+                    "METER_01EP7TQFBGUJKVTNP8V3FXMCE  "],
+        'Percentage%': [80, 90, 35, 55, 56],
+        'Expected': [12, 15, 12, 13, 10]
+        }
+    df = pd.DataFrame(details)            
+    gas_figure = go.Figure(go.Bar(
+        x=df["Expected"], y=df["MeterID"],
+        hovertemplate = 'MeterID: %{y}'+'<br>Expected: %{x}<br>'+'Percentage: %{text}',
+        text = [' {}%'.format(i) for i in df['Percentage%']],
+        orientation='h'
+    ))
+    return json.loads(gas_figure.to_json())
 
 @app.get("/graph-pure-plotly/{item_id}")
 def get_graph(item_id: int):
